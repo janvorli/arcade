39bf2a51a4aa9ff55324ceec2a49970fe2386b0c
Cesar Soares Lucas <disoares@microsoft.com>
2019-06-07 09:38:40 -0700

    Some refactorings

--- eng/common/release/dotnetsymbol-init.ps1
+++ eng/common/release/dotnetsymbol-init.ps1
@@ -2,7 +2,7 @@ param (
     $dotnetsymbolVersion = $null
 )
 
-$verbosity = "m"
+$verbosity = "minimal"
 . $PSScriptRoot\..\tools.ps1
 
 function Installdotnetsymbol ($dotnetsymbolVersion) {
@@ -10,15 +10,15 @@ function Installdotnetsymbol ($dotnetsymbolVersion) {
 
   $dotnetRoot = InitializeDotNetCli -install:$true
   $dotnet = "$dotnetRoot\dotnet.exe"
-  $toolList = & "$dotnet" tool list -g
+  $toolList = & "$dotnet" tool list --global
 
   if ($toolList -like "*$dotnetsymbolPackageName*") {
-    & "$dotnet" tool uninstall $dotnetsymbolPackageName -g
+    & "$dotnet" tool uninstall $dotnetsymbolPackageName --global
   }
  
   Write-Host "Installing dotnet-symbol version $dotnetsymbolVersion..."
   Write-Host "You may need to restart your command window if this is the first dotnet tool you have installed."
-  & "$dotnet" tool install $dotnetsymbolPackageName --version $dotnetsymbolVersion -v $verbosity -g
+  & "$dotnet" tool install $dotnetsymbolPackageName --version $dotnetsymbolVersion --verbosity $verbosity --global
 }
 
 Installdotnetsymbol $dotnetsymbolVersion
--- eng/common/release/sourcelink-cli-init.ps1
+++ eng/common/release/sourcelink-cli-init.ps1
@@ -2,7 +2,7 @@ param (
     $sourcelinkCliVersion = $null
 )
 
-$verbosity = "m"
+$verbosity = "minimal"
 . $PSScriptRoot\..\tools.ps1
 
 function InstallSourcelinkCli ($sourcelinkCliVersion) {
@@ -10,15 +10,15 @@ function InstallSourcelinkCli ($sourcelinkCliVersion) {
 
   $dotnetRoot = InitializeDotNetCli -install:$true
   $dotnet = "$dotnetRoot\dotnet.exe"
-  $toolList = & "$dotnet" tool list -g
+  $toolList = & "$dotnet" tool list --global
 
   if ($toolList -like "*$sourcelinkCliPackageName*") {
-    & "$dotnet" tool uninstall $sourcelinkCliPackageName -g
+    & "$dotnet" tool uninstall $sourcelinkCliPackageName --global
   }
  
   Write-Host "Installing SourceLink CLI version $sourcelinkCliVersion..."
   Write-Host "You may need to restart your command window if this is the first dotnet tool you have installed."
-  & "$dotnet" tool install $sourcelinkCliPackageName --version $sourcelinkCliVersion -v $verbosity -g
+  & "$dotnet" tool install $sourcelinkCliPackageName --version $sourcelinkCliVersion --verbosity $verbosity --global
 }
 
 InstallSourcelinkCli $sourcelinkCliVersion
